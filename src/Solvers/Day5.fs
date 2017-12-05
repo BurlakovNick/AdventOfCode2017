221e3efbd947041d0cd23fa84dd034475ac00dea
Burlakov Nick <burlakov.nick@gmail.com>
2017-12-05 21:12:41 +0500

    Day 5 final solution

--- src/Solvers/Day5.fs
+++ src/Solvers/Day5.fs
@@ -3,24 +3,20 @@ module Day5
 open System
 open System.Linq
 
-let doJumps (jumps: int[]) =
-    let mutable position = 0
-    [
-        while 0 <= position && position < jumps.Length do
-            let jumpLength = jumps.ElementAt position
-            jumps.SetValue(jumpLength + 1, position)
-    ]
+let infinity = [ while true do yield () ]
 
 let rec countJumps (jumps: int[]) =
     let (position, distance) =
-        infinity
-        |> Seq.map (fun position _ ->
+        Seq.initInfinite (fun i -> i)
+        |> Seq.scan (fun (position, distance) _ ->
              let jumpLength = jumps.ElementAt position
-             jumps.SetValue(jumpLength + 1, position)
-             position + jumpLength
-        ) 0
-        |> Seq.takeWhile (fun position -> position >= 0 && position < jumps.Length)
-    distance
+             let newJumpLength = if jumpLength >= 3 then jumpLength - 1 else jumpLength + 1
+             jumps.SetValue(newJumpLength, position)
+             (position + jumpLength, distance + 1)
+        ) (0, 0)
+        |> Seq.takeWhile (fun (position, distance) -> position >= 0 && position < jumps.Length)
+        |> Seq.last
+    distance + 1
 
 let solveJumps (lines : seq<string>) =
     let jumps = Array.ofSeq (Seq.map Int32.Parse lines)
