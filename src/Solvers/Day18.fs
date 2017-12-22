43223c236e56f9bad2613e40861e7fef4d862c7a
Burlakov Nick <burlakov.nick@gmail.com>
2017-12-22 23:13:10 +0500

    Day 18 try hard 2

--- src/Solvers/Day18.fs
+++ src/Solvers/Day18.fs
@@ -4,7 +4,7 @@ open System
 open System.Linq
 
 type Register = char
-type Integer = int
+type Integer = int64
 type Argument = Register of Register | Integer of Integer
 
 type Command = 
@@ -12,41 +12,51 @@ type Command =
     | Add of Register * Argument
     | Mul of Register * Argument
     | Mod of Register * Argument
-    | Snd of Register
-    | Rcv of Register
-    | Jgz of Register * Argument
+    | Snd of Argument
+    | Rcv of Argument
+    | Jgz of Argument * Argument
     | Nop
 
 type State = {
-    lastPlayed: Integer;
-    recovered: Integer;
     registers: Map<Register, Integer>;
     commands: Command[];
     currentCommand: int;
+    programId: int;
+    waiting: bool;
+    finished: bool;
+    sentCount: int;
+    sent: List<Integer>;
+    received: List<Integer>;
 }
     
 let parseArg (line: string) = 
     if Char.IsLetter line.[0] then Register line.[0]
-    else Integer (Int32.Parse line)
+    else Integer (Int64.Parse line)
+
+let toReg arg = 
+    match arg with
+    | Register reg -> Some reg
+    | Integer i -> None
 
 let parseCommand (line: string) = 
     let tokens = line.Split([| " " |], StringSplitOptions.RemoveEmptyEntries)
-    let reg = tokens.[1].[0]
-    let arg = if tokens.Length > 2 then Some (parseArg tokens.[2]) else None
+    let arg1 = parseArg tokens.[1]
+    let reg = toReg arg1
+    let arg2 = if tokens.Length > 2 then Some (parseArg tokens.[2]) else None
     
     match tokens.[0] with
-    | "set" -> Set(reg, arg.Value)
-    | "add" -> Add(reg, arg.Value)
-    | "mul" -> Mul(reg, arg.Value)
-    | "mod" -> Mod(reg, arg.Value)
-    | "snd" -> Snd(reg)
-    | "rcv" -> Rcv(reg)
-    | "jgz" -> Jgz(reg, arg.Value)
+    | "set" -> Set(reg.Value, arg2.Value)
+    | "add" -> Add(reg.Value, arg2.Value)
+    | "mul" -> Mul(reg.Value, arg2.Value)
+    | "mod" -> Mod(reg.Value, arg2.Value)
+    | "snd" -> Snd(arg1)
+    | "rcv" -> Rcv(arg1)
+    | "jgz" -> Jgz(arg1, arg2.Value)
     | _ -> Nop 
 
 let getRegister state reg = 
     if state.registers.ContainsKey reg then state.registers.Item reg
-    else 0
+    else 0L
 
 let getValue state argument = 
     match argument with
@@ -61,6 +71,9 @@ let modifyRegister (state: State) reg arg operation =
     }
 
 let execute (state: State) = 
+    if state.currentCommand = state.commands.Length then { state with finished = true; }
+    else
+    
     let command = state.commands.[state.currentCommand]
     let modify = modifyRegister state
 
@@ -70,46 +83,101 @@ let execute (state: State) =
             modify reg arg (fun reg arg -> arg)
         
         | Add (reg, arg) -> 
-            modify reg arg (fun reg arg -> reg + arg)
+            modify reg arg (+)
         
         | Mul (reg, arg) -> 
-            modify reg arg (fun reg arg -> reg * arg)
+            modify reg arg (*)
             
         | Mod (reg, arg) ->
-            modify reg arg (fun reg arg -> reg % arg)
+            modify reg arg (%)
         
-        | Snd (reg) ->
+        | Snd (arg) ->
             { 
-                state with lastPlayed = getRegister state reg;
+                state with 
+                    sent = Seq.append state.sent [ getValue state arg ] |> Seq.toList;
+                    sentCount = state.sentCount + 1
             }
         
         | Rcv (reg) ->
-            let regValue = getRegister state reg
-            if regValue <> 0 then { state with recovered = state.lastPlayed; }
-            else state
+            if state.received.IsEmpty then { state with waiting = true; }
+            else 
+                { 
+                    state with 
+                        received = Seq.skip 1 state.received |> Seq.toList;
+                        waiting = false;
+                        currentCommand = state.currentCommand + 1;
+                }
             
-        | Jgz (reg, arg) ->
-            let regValue = getRegister state reg
-            let argValue = getValue state arg
-            if regValue > 0 then { state with currentCommand = state.currentCommand + argValue }
+        | Jgz (arg1, arg2) ->
+            let regValue = getValue state arg1
+            let argValue = getValue state arg2
+            if regValue > 0L then { state with currentCommand = state.currentCommand + int argValue }
             else { state with currentCommand = state.currentCommand + 1 }
         
         | Nop -> state
-    
+
+    //printfn "%A" command
+    //printfn "%A" state
+    //printfn ""
+
     match command with
-    | Jgz (_, _) -> state
+    | Jgz _ -> state
+    | Rcv _ -> state
     | _ -> { state with currentCommand = state.currentCommand + 1 }
 
-let executeAll commands = 
-    let initState = { lastPlayed = 0; recovered = 0; registers = Map.empty; commands = commands; currentCommand = 0; }
+let createInitState (programId: int) commands = 
+    { 
+        registers = Map.ofList [ ('p', (int64) programId) ];
+        commands = commands;
+        currentCommand = 0;
+        programId = programId;
+        sent = List.empty;
+        sentCount = 0;
+        received = List.empty;
+        waiting = false;
+        finished = false;
+    }
     
-    Seq.initInfinite (fun _ -> ())
-    |> Seq.scan (fun state _ -> execute state) initState
-    |> Seq.skipWhile (fun state -> state.recovered = 0)
-    |> Seq.head
+let canExecute state = 
+    state.waiting = false && state.finished = false
+
+let tryExecuteOneStep (state0, state1) = 
+    if canExecute state1 then
+        let state1 = execute state1
+        let state0 = 
+            {
+                state0 with
+                    waiting = if state0.waiting then state1.sent.IsEmpty = false else false;
+                    received = state1.sent;
+            }
+        (state0, state1)
+    else
+    if canExecute state0 then
+        let state0 = execute state0
+        let state1 = 
+            {
+                state1 with
+                    waiting = if state1.waiting then state0.sent.IsEmpty = false else false;
+                    received = state0.sent;
+            }
+        (state0, state1)
+    else
+        (state0, state1)
+
+let executeBothPrograms commands = 
+    let state0 = createInitState 0 commands
+    let state1 = createInitState 1 commands
+    
+    let (state0, state1) = 
+        Seq.initInfinite (fun _ -> ())
+        |> Seq.scan (fun (x, y) _ -> tryExecuteOneStep (x, y)) (state0, state1)
+        |> Seq.skipWhile (fun (x, y) -> canExecute x || canExecute y)
+        |> Seq.head
+        
+    state1.sentCount
 
 let solveDuet (lines : seq<string>) =
-    //old: 862
+    //old: 127 - to low
     let commands = lines |> Seq.map parseCommand |> Seq.toArray
-    let state = executeAll commands
-    state.recovered.ToString()
+    let answer = executeBothPrograms commands
+    answer.ToString()
